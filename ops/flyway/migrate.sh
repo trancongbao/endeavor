0cffdc13495a9ca15dd16c7bb3ed2465277533aa
trancongbao <trancongbao@gmail.com>
2026-01-04 12:42:43 +0700

    Postgres Security

--- ops/flyway/migrate.sh
+++ ops/flyway/migrate.sh
@@ -13,33 +13,37 @@ set -a
 source "$SCRIPT_DIR/.env"
 set +a
 
-# Create flyway user if it does not exist
+# Create flyway user everytime to ensure clean state
 export PGPASSWORD="$POSTGRES_PASSWORD"
 psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
-<<-EOSQL  
-  DO \$\$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flyway') THEN
-      CREATE ROLE flyway LOGIN NOINHERIT PASSWORD '${FLYWAY_USER_PWD}';
-    END IF;
-  END
-  \$\$;
+<<-EOSQL
+DO \$\$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flyway') THEN
+    RAISE NOTICE 'Dropping existing flyway role';
+    REASSIGN OWNED BY flyway TO CURRENT_USER;
+    DROP OWNED BY flyway;
+    DROP ROLE flyway;
+  END IF;
 
-  ALTER ROLE flyway
-    NOSUPERUSER
-    NOCREATEDB
-    NOCREATEROLE
-    NOREPLICATION
-    NOBYPASSRLS;
+  RAISE NOTICE 'Creating flyway role';
+  EXECUTE format(
+    'CREATE ROLE %I LOGIN PASSWORD %L NOINHERIT CREATEROLE',
+    'flyway',
+    '${FLYWAY_PASSWORD}'
+  );
 
-  GRANT CONNECT ON DATABASE ${POSTGRES_DB} TO flyway;
-  GRANT USAGE, CREATE ON SCHEMA public TO flyway;
+  -- Grant basic database access
+  EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), 'flyway');
+  EXECUTE format('GRANT USAGE, CREATE ON SCHEMA public TO %I', 'flyway');
+END
+\$\$;
 EOSQL
 
 # Run Flyway migrations
 docker run --rm \
   --network endeavor_default \
-  --env-file ops/flyway/.flyway.env \
+  --env-file .env \
   -e FLYWAY_URL=jdbc:postgresql://postgres:5432/$POSTGRES_DB \
-  -v "$PWD/ops/flyway/migrations:/flyway/sql" \
-  flyway/flyway migrate
+  -v "$PWD/migrations:/flyway/sql" \
+  flyway/flyway:11-alpine migrate
