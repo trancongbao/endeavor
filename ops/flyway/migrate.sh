a83a1ec5af856c04eb9b0844289aa64dadeb6a4b
trancongbao <trancongbao@gmail.com>
2026-01-04 14:56:09 +0700

    Postgres Security

--- ops/flyway/migrate.sh
+++ ops/flyway/migrate.sh
@@ -13,37 +13,35 @@ set -a
 source "$SCRIPT_DIR/.env"
 set +a
 
-# Create flyway user everytime to ensure clean state
+# Create flyway user if not exists
 export PGPASSWORD="$POSTGRES_PASSWORD"
 psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
 <<-EOSQL
-DO \$\$
-BEGIN
-  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flyway') THEN
-    RAISE NOTICE 'Dropping existing flyway role';
-    REASSIGN OWNED BY flyway TO CURRENT_USER;
-    DROP OWNED BY flyway;
-    DROP ROLE flyway;
-  END IF;
+  -- Create the flyway role if it doesn't exist
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flyway') THEN
+      CREATE ROLE flyway LOGIN NOINHERIT CREATEROLE;
+    END IF;
+  END
+  \$\$;
 
-  RAISE NOTICE 'Creating flyway role';
-  EXECUTE format(
-    'CREATE ROLE %I LOGIN PASSWORD %L NOINHERIT CREATEROLE',
-    'flyway',
-    '${FLYWAY_PASSWORD}'
-  );
+  -- Always update the password
+  ALTER ROLE flyway WITH PASSWORD '${FLYWAY_PASSWORD}';
 
   -- Grant basic database access
-  EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), 'flyway');
-  EXECUTE format('GRANT USAGE, CREATE ON SCHEMA public TO %I', 'flyway');
-END
-\$\$;
+  GRANT CONNECT ON DATABASE $POSTGRES_DB TO flyway;
+  GRANT CREATE ON DATABASE $POSTGRES_DB TO flyway;
+
+  CREATE SCHEMA IF NOT EXISTS flyway;
+  GRANT USAGE, CREATE ON SCHEMA flyway TO flyway;
 EOSQL
 
 # Run Flyway migrations
 docker run --rm \
   --network endeavor_default \
   --env-file .env \
-  -e FLYWAY_URL=jdbc:postgresql://postgres:5432/$POSTGRES_DB \
+  -e FLYWAY_URL=jdbc:postgresql://postgres:$POSTGRES_PORT/$POSTGRES_DB \
+  -e FLYWAY_SCHEMAS=flyway \
   -v "$PWD/migrations:/flyway/sql" \
   flyway/flyway:11-alpine migrate
--- ops/flyway/migrations/V1_1__tables.sql
+++ ops/flyway/migrations/V1_1__tables.sql
@@ -1,24 +1,4 @@
-/***********************************************************************************************************************
-
-   SSSSSSSSSSSSSSS                     hhhhhhh
- SS:::::::::::::::S                    h:::::h
-S:::::SSSSSS::::::S                    h:::::h
-S:::::S     SSSSSSS                    h:::::h
-S:::::S                cccccccccccccccc h::::h hhhhh           eeeeeeeeeeee       mmmmmmm    mmmmmmm     aaaaaaaaaaaaa
-S:::::S              cc:::::::::::::::c h::::hh:::::hhh      ee::::::::::::ee   mm:::::::m  m:::::::mm   a::::::::::::a
- S::::SSSS          c:::::::::::::::::c h::::::::::::::hh   e::::::eeeee:::::eem::::::::::mm::::::::::m  aaaaaaaaa:::::a
-  SS::::::SSSSS    c:::::::cccccc:::::c h:::::::hhh::::::h e::::::e     e:::::em::::::::::::::::::::::m           a::::a
-    SSS::::::::SS  c::::::c     ccccccc h::::::h   h::::::he:::::::eeeee::::::em:::::mmm::::::mmm:::::m    aaaaaaa:::::a
-       SSSSSS::::S c:::::c              h:::::h     h:::::he:::::::::::::::::e m::::m   m::::m   m::::m  aa::::::::::::a
-            S:::::Sc:::::c              h:::::h     h:::::he::::::eeeeeeeeeee  m::::m   m::::m   m::::m a::::aaaa::::::a
-            S:::::Sc::::::c     ccccccc h:::::h     h:::::he:::::::e           m::::m   m::::m   m::::ma::::a    a:::::a
-SSSSSSS     S:::::Sc:::::::cccccc:::::c h:::::h     h:::::he::::::::e          m::::m   m::::m   m::::ma::::a    a:::::a
-S::::::SSSSSS:::::S c:::::::::::::::::c h:::::h     h:::::h e::::::::eeeeeeee  m::::m   m::::m   m::::ma:::::aaaa::::::a
-S:::::::::::::::SS   cc:::::::::::::::c h:::::h     h:::::h  ee:::::::::::::e  m::::m   m::::m   m::::m a::::::::::aa:::a
- SSSSSSSSSSSSSSS       cccccccccccccccc hhhhhhh     hhhhhhh    eeeeeeeeeeeeee  mmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa
-
-Source: https://patorjk.com/software/taag/#p=display&h=0&v=0&f=Doh&t=Data
-***********************************************************************************************************************/
+SET search_path TO app;
 
 CREATE DOMAIN email AS TEXT
     CHECK (value ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
